a5e8c798ab4f46d35dd6e428dafb92d6477ca0a5
Ajay Kumar <ajayaaaapp@gmail.com>
2024-04-23 05:51:43 +0530

    Mongodb setup

--- BlogProjects/API/index.js
+++ BlogProjects/API/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://Ajaycloud:Ajaycloud1234@ajaycloud.bncjx7d.mongodb.net/MernBlog?retryWrites=true&w=majority&appName=Ajaycloud").then(() => {
+    console.log("database is connected");
+}).catch((err) => {
+    console.log(err);
+})
 const app = express();
 
-app.listen("3000",()=>{
+app.listen("3000", () => {
     console.log("server is running at 3000");
 })
